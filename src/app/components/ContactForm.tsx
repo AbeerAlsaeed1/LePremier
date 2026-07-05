a25f8ac30969ec3c7cdbc49a129a4f76020920a1
abeer <abeer.alsaeed@lepremier.com.sa>
2026-07-05 10:10:45 +0300

    Connect contact form

---
+++ src/app/components/ContactForm.tsx
@@ -0,0 +1,35 @@
+export default function ContactForm() {
+  return (
+    <form action="https://api.web3forms.com/submit" method="POST">
+
+      <input
+        type="hidden"
+        name="access_key"
+        value="38e0b390-6d79-4e7b-8753-a902507bfb90"
+      />
+
+      <input
+        type="hidden"
+        name="redirect"
+        value="https://lepremier-group.com"
+      />
+
+      <h2>Contact Us</h2>
+
+      <input type="text" name="name" placeholder="Your Name" required />
+      <br /><br />
+
+      <input type="email" name="email" placeholder="Your Email" required />
+      <br /><br />
+
+      <input type="text" name="subject" placeholder="Subject" />
+      <br /><br />
+
+      <textarea name="message" placeholder="Message" required />
+      <br /><br />
+
+      <button type="submit">Send Message</button>
+
+    </form>
+  );
+}
